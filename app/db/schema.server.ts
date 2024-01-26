f48bda8727f4d681e2fb25f462f5174cf74a2124
mrks <hey@mrks.me>
2024-01-26 12:53:12 +0100

    feat: working form

--- app/db/schema.server.ts
+++ app/db/schema.server.ts
@@ -3,7 +3,6 @@ import { text, sqliteTable, integer } from "drizzle-orm/sqlite-core";
 export const entries = sqliteTable('entries', {
   id: integer('id', {mode: 'number'}).primaryKey({ autoIncrement: true }),
   category: text("category").default("work"),
-  title: text("title").notNull(),
   description: text("description"),
   date: text("date")
     .notNull()
